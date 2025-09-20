d2b41c2852f5d85b64fd2230e996f115b7ee13ac
Sarukesh0920 <sarukesh54@gmail.com>
2025-09-20 11:42:12 +0530

    Using nested for

--- Placement Pratice Programs/Patten Prg1.py
+++ Placement Pratice Programs/Patten Prg1.py
@@ -1,4 +1,6 @@
-for i in range (1,6):
+n = 5
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
     print()
-    for j in range(1,i+1):
-        print('*',end=" ")
--- Placement Pratice Programs/Patten Prg2.py
+++ Placement Pratice Programs/Patten Prg2.py
@@ -1,4 +1,8 @@
-n = 5
+n = 3
 
 for i in range(1, n + 1):
-    print(" " * (n - i) + "*" * (2 * i - 1))
+    for s in range(n - i):
+        print(" ", end="")
+    for j in range(2 * i - 1):
+        print("*", end="")
+    print()
